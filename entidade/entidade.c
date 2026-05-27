dcba4336ac5cf4f6526ef268d82019931981033d
DannDdC <danieldc0106@gmail.com>
2026-05-26 21:50:30 -0300

    criação das funções de adicionar entidade no início da lista, remover entidade em qualquer posição da lista e apagar a lista; include no main.c

--- entidade/entidade.c
+++ entidade/entidade.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <raylib.h>
-#include <entidade.h>
+#include "entidade.h"
 
 ListaEntidades* lista_criar(void) { //criação de lista encadeada pras entidades
     ListaEntidades* lista = malloc(sizeof(ListaEntidades));
@@ -52,4 +53,65 @@ Entidade* entidade_criar(EntTipo tipo, int x, int y) { //função de criar entid
     }
 
     return entidade;
+}
+
+
+void lista_adicionar(ListaEntidades* lista, Entidade* entidade) { //adiciona uma entidade no INÍCIO da lista. EX.: se antes era A->B->NULL, agora vira N->A->B->NULL
+    if (lista == NULL || entidade == NULL) { //lenguisa B-)
+        return;
+    }
+
+    entidade->next = lista->head;
+    lista->head = entidade;
+
+    lista->contagem++; //debug, aumenta o contador de entidades na lista
+}
+
+void lista_remover(ListaEntidades* lista, Entidade* alvo) { //remove uma entidade em qualquer lugar da lista e da free naquele espaço de memória
+    if (lista == NULL || alvo == NULL) { //sarsissa 8-)
+        return;
+    }
+
+    Entidade* atual = lista->head;
+    Entidade* anterior = NULL; //preciso do anterior pra quando der free, ele apontar pro que vinha depois da entidade removida. Começa como NULL pq não tem anterior ainda
+
+    while (atual != NULL) {
+        if (atual == alvo) {
+            if (anterior == NULL) { //pro caso do atual ser o primeiro item da lista (anterior não existe (NULL))
+                lista->head = atual->next;
+            }
+            else { 
+                anterior->next = atual->next; //anterior agora aponta pra casa depois do atual, "pulando" a casa atual
+            }
+
+            free(atual); //atual desaparece pra sempre
+            lista->contagem--; //debug, reduz o contador de entidades da lista
+
+            return;
+        }
+
+        anterior = atual;
+        atual = atual->next;
+    }
+}
+
+void lista_limpar(ListaEntidades* lista) { //apaga a lista e libera o espaço alocado pra ela, muito mais simples que lista_remover
+    if (lista == NULL) { //çalame :S
+        return;
+    }
+
+    Entidade* atual = lista->head; //atual aponta pro primeiro item da lista
+
+    while (atual != NULL) { //enquanto atual não for vazio, ele continua dando free no atual e passando pra próxima casa
+        Entidade* proximo = atual->next;
+
+        free(atual);
+
+        atual = proximo;
+    }
+
+    lista->head = NULL;
+    lista->contagem = 0;
+
+    free(lista);
 }
